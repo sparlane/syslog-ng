sparlane/syslog-ng

commit e788e357809ad8db3db4a83bbd32c3af9a00305e

CC-420: Add syslogs for TLS in syslog-ng
According to the Common Criteria requirements, TLS state changes should
be logged to syslog.  This adds the approriate syslog calls to syslog-ng.

Change-Id: I266827c25b9f941de963ab3a070d7f253579ce33
Reviewed-by: Ken Kato <ken_kato@alliedtelesis.com>
Reviewed-by: John Culbreth <john_culbreth@alliedtelesis.com>
Reviewed-by: David Thomson <david.thomson@alliedtelesis.co.nz>
Reviewed-by: Anthony Lineham <anthony.lineham@alliedtelesis.co.nz>
Signed-off-by: Anthony Lineham <anthony.lineham@alliedtelesis.co.nz>

# Conflicts:
#	lib/tlscontext.c

lib/tlscontext.c
@@ -642,6 +642,24 @@ tls_session_new(SSL *ssl, TLSContext *ctx)
 void
 tls_session_free(TLSSession *self)
 {
+#ifdef ATL_CHANGE
+  GString *subject_name;
+  X509* peer_cert;
+  int state = SSL_get_state(self->ssl);
+
+  /* If we are transitioning from a good state then output a syslog */
+  if (SSL_ST_ERR != state)
+    {
+      subject_name = g_string_sized_new(128);
+      peer_cert = SSL_get_peer_certificate(self->ssl);
+      if(peer_cert)
+        {
+          tls_x509_format_dn(X509_get_subject_name(peer_cert), subject_name);
+          msg_notice ("TLS session ended", evt_tag_str("session", subject_name->str));
+        }
+      g_string_free(subject_name, TRUE);
+    }
+#endif
   tls_context_unref(self->ctx);
   if (self->verifier)
     tls_verifier_unref(self->verifier);

lib/transport/transport-tls.c
@@ -106,12 +106,38 @@ log_transport_tls_write_method(LogTransport *s, const gpointer buf, gsize buflen
   gint ssl_error;
   gint rc;
 
+#ifdef ATL_CHANGE
+  GString *subject_name;
+  X509* peer_cert;
+  int old_state = SSL_get_state(self->tls_session->ssl);
+#endif
+
   /* assume that we need to poll our output for writing unless
    * SSL_ERROR_WANT_READ is specified by libssl */
 
   self->super.cond = G_IO_OUT;
 
   rc = SSL_write(self->tls_session->ssl, buf, buflen);
+
+#ifdef ATL_CHANGE
+  /* If we are transitioning to a connected state then output a syslog */
+  if ((SSL_ST_OK != old_state) && (SSL_ST_OK == SSL_get_state(self->tls_session->ssl)))
+    {
+      subject_name = g_string_sized_new(128);
+      peer_cert = SSL_get_peer_certificate(self->tls_session->ssl);
+      if(peer_cert)
+        {
+          tls_x509_format_dn(X509_get_subject_name(peer_cert), subject_name);
+        }
+      else
+        {
+          g_string_assign (subject_name, "UNKNOWN");
+        }
+      msg_notice ("Syslog-ng TLS session initiated",
+                  evt_tag_str("subject", subject_name->str));
+      g_string_free(subject_name, TRUE);
+    }
+#endif
 
   if (rc < 0)
     {

commit 546e7e814aa2e76a319b6024c49090dce3a52424

CR-75844: Free SSL peer certificates
When calling SSL_get_peer_certificate() we must call X509_free on the
returned certificate file to prevent a memory leak.

Fixes: e788e3578 ("CC-420: Add syslogs for TLS in syslog-ng")
Change-Id: I057962770b3b4af9eb9708534805f48dd2fabed7
Reviewed-by: Evan Nimmo <evan.nimmo@alliedtelesis.co.nz>
Reviewed-by: Anthony Lineham <anthony.lineham@alliedtelesis.co.nz>
Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>

lib/tlscontext.c
@@ -657,6 +657,7 @@ tls_session_free(TLSSession *self)
         {
           tls_x509_format_dn(X509_get_subject_name(peer_cert), subject_name);
           msg_notice ("TLS session ended", evt_tag_str("session", subject_name->str));
+          X509_free (peer_cert);
         }
       g_string_free(subject_name, TRUE);
     }

lib/transport/transport-tls.c
@@ -136,6 +136,7 @@ log_transport_tls_write_method(LogTransport *s, const gpointer buf, gsize buflen
       if(peer_cert)
         {
           tls_x509_format_dn(X509_get_subject_name(peer_cert), subject_name);
+          X509_free (peer_cert);
         }
       else
         {